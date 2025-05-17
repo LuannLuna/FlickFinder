75e01d89dd36a2e45f41f9104eb4cf7100a9b27b
luannluna <luann.marques@gmail.com>
2025-05-17 14:50:16 -0300

    created FlickFinder project

---
+++ FlickFinder/FlickFinderTests/FlickFinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlickFinderTests.swift
+//  FlickFinderTests
+//
+//  Created by Luann Luna on 17/05/25.
+//
+
+import Testing
+@testable import FlickFinder
+
+struct FlickFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
